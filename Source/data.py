0cae8d145996b5b0c903292f3288e61f989e9fc1
Ivan Pikulin <pikulin.ivan.pelikan@gmail.com>
2023-05-02 12:37:36 +0300

    tmp commit

--- Source/data.py
+++ Source/data.py
@@ -1,7 +1,32 @@
+import random
+
 from sklearn.model_selection import KFold, train_test_split
-from torch.utils.data import DataLoader
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
+
+
+def balanced_train_test_valid_split(datasets, n_folds, batch_size, shuffle_every_epoch, valid_size=None, seed=17):
+    train = [[] for _ in range(n_folds)]
+    val = [[] for _ in range(n_folds)]
+    for dataset in datasets:
+        dataset = (dataset * n_folds)[:n_folds] if len(dataset) < n_folds else dataset
+        mol_ids = list(range(len(dataset)))
+        if n_folds == 1:
+            train_index, valid_index = train_test_split(mol_ids, test_size=valid_size, random_state=seed, shuffle=False)
+            train[0] += [val for i, val in enumerate(dataset) if i in train_index]
+            val[0] += [val for i, val in enumerate(dataset) if i in valid_index]
+        else:
+            for fold_ind, (train_index, valid_index) in enumerate(KFold(n_splits=n_folds).split(mol_ids)):
+                train[fold_ind] += [val for i, val in enumerate(dataset) if i in train_index]
+                val[fold_ind] += [val for i, val in enumerate(dataset) if i in valid_index]
+                random.Random(seed).shuffle(train[fold_ind])
+                random.Random(seed).shuffle(val[fold_ind])
+
+    train_loaders = [DataLoader(train[fold_ind], batch_size=batch_size, shuffle=shuffle_every_epoch)
+                     for fold_ind in range(n_folds)]
+    valid_loaders = [DataLoader(val[fold_ind], batch_size=batch_size, shuffle=shuffle_every_epoch)
+                     for fold_ind in range(n_folds)]
+    return list(zip(train_loaders, valid_loaders))
 
 
 def train_test_valid_split(dataset, n_splits=5, test_ratio=0.2, batch_size=64, seed=14):
--- Source/models/GCNN_bimodal/train_GCNN.py
+++ Source/models/GCNN_bimodal/train_GCNN.py
@@ -10,6 +10,7 @@ from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 from torch import nn
 from torch_geometric.nn import global_mean_pool, MFConv
 
+from Source.featurizers.featurizers import featurize_sdf_with_metal, SkipatomFeaturizer
 from Source.models.global_poolings import ConcatPooling
 
 sys.path.append(os.path.abspath("."))
@@ -27,7 +28,15 @@ epochs = 1000
 es_patience = 100
 mode = "regression"
 output_folder = f"Output/TestGCNN_IC50_{mode}_{time_mark}"
-path_to_csv = "Data/IC50.csv"
+train_sdf = "Data/OneM"
+test_sdf = ""
+test_metal = sys.argv[1]
+
+transition_metals = ["Mg", "Al", "Ca", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Y", "Ag", "Cd", "Hg", "Pb", "Bi"]
+Ln_metals = ["La", "Ce", "Pr", "Nd", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu"]
+Ac_metals = ["Th", "Am", "Cm", "Bk", "Cf"]  # "U", "Pu",
+all_metals = transition_metals + Ln_metals + Ac_metals
+
 max_data = None
 targets = ("logK",)
 target_metrics = {
@@ -40,6 +49,33 @@ target_metrics = {
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 logging.info("Featurizig...")
+
+balanced_train_test_valid_split
+
+test_loader = DataLoader(featurize_sdf_with_metal(
+    path_to_sdf=test_sdf,
+    mol_featurizer=ConvMolFeaturizer(),
+    metal_featurizer=SkipatomFeaturizer("Source/featurizers/skipatom_vectors_dim200.json")),
+    batch_size=batch_size)
+
+model = MolGraphHeteroNet(
+    node_features=get_num_node_features(folds[0][0]),
+    metal_features=get_num_metal_features(folds[0][0]),
+    num_targets=get_num_targets(folds[0][0]),
+    batch_size=get_batch_size(folds[0][0]),
+    **model_parameters,
+)
+trainer = MolGraphHeteroNetTrainer(
+    model=model,
+    train_valid_data=folds,
+    test_data=test_loader,
+    output_folder=output_path,
+    out_folder_mark=output_mark,
+    epochs=epochs,
+    es_patience=es_patience,
+    seed=seed)
+trainer.train_cv_models()
+
 data = featurize_csv_to_graph(path_to_csv=path_to_csv,
                               targets=targets,
                               featurizer=DGLFeaturizer(add_self_loop=True,
